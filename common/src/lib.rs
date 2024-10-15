3ee60816b4188c66bc2ce0f4a88812f6c6198459
Kevin Joseph <kevinjoseph1995@gmail.com>
2024-10-14 21:10:10 -0700

    Fix a bug in CFG construction
    Add a method to dump a DOT representation of
    a CFG for visualization.

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -1,21 +1,22 @@
 use std::{collections::HashMap, fmt::Debug};
 
 use bril_rs::{Code, Instruction};
+use indoc::indoc;
 use smallvec::SmallVec;
 
 #[derive(Debug)]
 pub struct BasicBlock<'a> {
-    name: String,
-    instruction_stream: &'a [Code],
+    pub name: String,
+    pub instruction_stream: &'a [Code],
 }
 
 #[derive(Default)]
 pub struct Cfg<'a> {
-    adjacency_list_per_vertex: Vec<SmallVec<[usize; 4]>>,
-    underlying_basic_blocks: &'a [BasicBlock<'a>],
+    pub adjacency_list_per_vertex: Vec<SmallVec<[usize; 4]>>,
+    pub underlying_basic_blocks: &'a [BasicBlock<'a>],
 }
 
-fn construct_cfg<'a>(basic_blocks: &'a [BasicBlock<'a>]) -> Option<Cfg<'a>> {
+pub fn construct_cfg<'a>(basic_blocks: &'a [BasicBlock<'a>]) -> Option<Cfg<'a>> {
     if basic_blocks.is_empty() {
         return None;
     }
@@ -32,7 +33,7 @@ fn construct_cfg<'a>(basic_blocks: &'a [BasicBlock<'a>]) -> Option<Cfg<'a>> {
     let mut adjacency_list_per_vertex: Vec<SmallVec<[usize; 4]>> = Vec::new();
     adjacency_list_per_vertex.reserve(basic_blocks.len());
 
-    for block in (&basic_blocks).iter() {
+    for (index, block) in (&basic_blocks).iter().enumerate() {
         let last_instruction_in_block = {
             match block
                 .instruction_stream
@@ -54,11 +55,16 @@ fn construct_cfg<'a>(basic_blocks: &'a [BasicBlock<'a>]) -> Option<Cfg<'a>> {
                 pos: _,
             } => (op, labels),
             _ => {
-                adjacency_list_per_vertex.push(SmallVec::new());
+                if index < basic_blocks.len() - 1 {
+                    adjacency_list_per_vertex.push(smallvec::smallvec![index + 1]);
+                }
                 continue;
             }
         };
         match effect_instruction_op {
+            bril_rs::EffectOps::Return => {
+                adjacency_list_per_vertex.push(smallvec::smallvec![]);
+            }
             bril_rs::EffectOps::Jump => {
                 assert!(
                     destination_labels.len() == 1,
@@ -110,7 +116,31 @@ impl<'a> std::fmt::Display for Cfg<'a> {
     }
 }
 
-fn construct_basic_block_stream<'a>(instructions: &'a [Code]) -> Vec<BasicBlock<'a>> {
+impl<'a> Cfg<'a> {
+    pub fn get_dot_representation(&self) -> String {
+        let mut output: String = "digraph{".to_string();
+        for block in self.underlying_basic_blocks {
+            output.push_str(&block.name);
+            output.push_str(" [label=\"");
+            output.push_str(&block.name);
+            output.push_str("\", shape=box];");
+        }
+        for (index, children) in self.adjacency_list_per_vertex.iter().enumerate() {
+            if !children.is_empty() {
+                for child in children {
+                    output.push_str(&self.underlying_basic_blocks[index].name);
+                    output.push_str(" -> ");
+                    output.push_str(&self.underlying_basic_blocks[*child].name);
+                    output.push(';');
+                }
+            }
+        }
+        output.push('}');
+        return output;
+    }
+}
+
+pub fn construct_basic_block_stream<'a>(instructions: &'a [Code]) -> Vec<BasicBlock<'a>> {
     let mut current_basic_block_start: usize = 0;
     let mut blocks: Vec<BasicBlock<'a>> = Vec::new();
     let get_current_label = |index: usize, current_label: Option<&str>| -> String {
@@ -124,10 +154,12 @@ fn construct_basic_block_stream<'a>(instructions: &'a [Code]) -> Vec<BasicBlock<
     for (index, instruction) in instructions.iter().enumerate() {
         match instruction {
             Code::Label { label, pos: _ } => {
-                blocks.push(BasicBlock {
-                    name: get_current_label(index, current_label.clone()),
-                    instruction_stream: &instructions[current_basic_block_start..index],
-                });
+                if index != current_basic_block_start {
+                    blocks.push(BasicBlock {
+                        name: get_current_label(index, current_label.clone()),
+                        instruction_stream: &instructions[current_basic_block_start..index],
+                    });
+                }
 
                 current_label = Some(label.as_str());
                 current_basic_block_start = index + 1;
@@ -179,13 +211,32 @@ mod tests {
     use indoc::indoc;
 
     const BRIL_PROGRAM_TEXT: &'static str = indoc! {r#"
-        @main {
-            v: int = const 4;
-            jmp .somewhere;
-            v: int = const 2;
-        .somewhere:
-            print v;
-        }"#};
+    @main(b0: bool, b1: bool) {
+        jmp .start;
+    .end:
+        print x_0_2;
+        print x_1_2;
+        ret;
+    .l_1_3:
+        jmp .end;
+    .l_1_2:
+        x_1_2 : int = const 0;
+        jmp .l_1_3;
+    .l_1_1:
+        x_1_1 : int = const 1;
+        jmp .l_1_3;
+    .l_0_3:
+        br b1 .l_1_1 .l_1_2;
+    .l_0_2:
+        x_0_2 : int = const 2;
+        jmp .l_0_3;
+    .l_0_1:
+        x_0_1 : int = const 3;
+        jmp .l_0_3;
+    .start:
+        br b0 .l_0_1 .l_0_2;
+    }
+"#};
 
     fn get_test_program() -> Program {
         let parser = bril2json::bril_grammar::AbstractProgramParser::new();
@@ -205,7 +256,8 @@ mod tests {
     fn test_block_map_formation() {
         let program = get_test_program();
         for function in &program.functions {
-            let _ = construct_basic_block_stream(&function.instrs);
+            let blocks = construct_basic_block_stream(&function.instrs);
+            println!("{:#?}", blocks);
         }
     }
 
@@ -216,6 +268,8 @@ mod tests {
             let basic_blocks = construct_basic_block_stream(&function.instrs);
             let cfg = construct_cfg(&basic_blocks);
             assert!(cfg.is_some());
+            let cfg = cfg.unwrap();
+            println!("{}", cfg.get_dot_representation());
         }
     }
 }
